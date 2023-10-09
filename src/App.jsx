1bc8cb59e04439933d8d6b66df7fb089d2e9fb93
Israfil0001 <147426648+Israfil0001@users.noreply.github.com>
2023-10-09 23:35:35 +0600

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import Message from "./components/Message/Message";
 import Search from "./components/Search/Search";
 import TopBar from "./components/TopBar/TopBar";
 import Home from "./components/home/Home";
+import "./style.css";
 function App() {
   return (
     <div>
       <TopBar />
-      <Search />
       <CardList />
-      <Message/>
-
+      <Message />
     </div>
   );
 }
