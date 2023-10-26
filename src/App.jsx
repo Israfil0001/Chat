97e600b91d4241e926128e063dc7d705321ea950
Israfil0001 <147426648+Israfil0001@users.noreply.github.com>
2023-10-26 22:18:13 +0600

    Hw

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+import { Route, Router, Routes } from "react-router-dom";
 import CardList from "./components/CardList/CardList";
 import Message from "./components/Message/Message";
 import Search from "./components/Search/Search";
 import TopBar from "./components/TopBar/TopBar";
 import Home from "./components/home/Home";
 import "./style.css";
+import Layout from "./components/Layout/Layout";
+
 function App() {
   return (
-    <div>
-      <TopBar />
-      <CardList />
-      <Message />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/Message" element={<Message />} />
+        <Route path="/CardList" element={<CardList />} />
+       
+
+      </Route>
+    </Routes>
   );
 }
 
